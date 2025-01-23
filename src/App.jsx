13aaea73d0c3e635817266c7c13a39c84d501c3e
jess-icaa <jessicashalominselvan@JESSICAs-Laptop.local>
2025-01-23 13:03:31 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BookCard from './components/bookcard';
 
 function App() {
   const books = [
@@ -34,7 +35,25 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  const App = () => {
+    return (
+      <div className='app'>
+        <h1>Digital Library</h1>
+        <div className='book-list'>
+          {booksData.map((book, index) => (
+            <BookCard
+            key={index}
+            title={book.title}
+            author={book.author}
+            description={book.description}
+            coverImage={book.coverImage}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
+``
--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,17 @@
 // write the book component code here
+import React from 'react';
+
+const BookCard = ({ title, author, description, coverImage }) => {
+    return (
+        <div className='book-card'>
+            <img src={coverImage} alt={title} />
+            <div className='book-details'>
+                <h2 className='book-title'>{title}</h2>
+                <p className='book-author'>by {author}</p>
+                <p className='book-description'>{description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
